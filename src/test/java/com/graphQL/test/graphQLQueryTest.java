6c01621896927a08ff0042ae0378784de4c0b370
moniljoshi <monil.joshi@qualitia.com>
2021-08-18 16:33:52 +0530

    Added fitst rest assured test for Post call

--- src/test/java/com/graphQL/test/graphQLQueryTest.java
+++ src/test/java/com/graphQL/test/graphQLQueryTest.java
@@ -6,6 +6,19 @@ import org.testng.annotations.Test;
 import static io.restassured.RestAssured.given;
 
 public class graphQLQueryTest {
+    @Test
+    public void getAllFilms(){
+        RestAssured.baseURI ="https://swapi-graphql.netlify.app";
+        String query = "{\"query\":\"{\\n  allFilms{\\n    films{\\n      title\\n    }\\n  }\\n}\",\"variables\":null}";
+        given().log().all()
+                .contentType("application/json")
+                .body(query)
+                .when().log().all()
+                .post("/.netlify/functions/index").
+                then().log().all()
+                .assertThat().statusCode(200);
+
+
 
     }
 }
