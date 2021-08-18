e801a37059bdc7262f6834802a0b89a7ccd25525
moniljoshi <monil.joshi@qualitia.com>
2021-08-18 16:33:13 +0530

    Added required project dependecies | Rest assured and TestNG

---
+++ src/test/java/com/graphQL/test/graphQLQueryTest.java
@@ -0,0 +1,11 @@
+package com.graphQL.test;
+
+import io.restassured.RestAssured;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class graphQLQueryTest {
+
+    }
+}
